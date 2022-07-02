b21df4c843f3d08cee2619d91a7037597bf78635
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-02 21:23:44 +0300

    feat: add menu card component

---
+++ src/components/MainMenu/MenuCard.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+interface MenuCardProps {
+    readonly backgroundColor: string
+}
+
+export const MenuCard = styled.div<MenuCardProps>`
+  box-sizing: border-box;
+  width: 138px;
+  height: 198px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({backgroundColor}) => backgroundColor};
+  border: 4px solid rgba(255, 255, 255, 0.6);
+  border-radius: 20px;
+  transition: all 0.2s;
+
+  &:hover {
+    border: 4px solid #FFFFFF;
+  }
+
+  &:active {
+    border: 4px solid #FBE0DC;
+  }
+`;
