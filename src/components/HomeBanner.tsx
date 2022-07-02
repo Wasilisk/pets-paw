633595a8720ec6677cc2fc0abe803b1a91a16598
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-02 21:25:29 +0300

    feat: add home banner component

---
+++ src/components/HomeBanner.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from "styled-components";
+import MainBanner from '../assets/images/main-banner.png'
+
+const HomeBannerContainer = styled.div`
+  width: 680px;
+  height: 840px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #FBE0DC;
+  border-radius: 20px;
+  margin-right: 18px;
+`
+
+const HomeBanner = () => {
+    return (
+        <HomeBannerContainer>
+            <img src={MainBanner} alt="Main banner"/>
+        </HomeBannerContainer>
+    );
+};
+
+export default HomeBanner;
