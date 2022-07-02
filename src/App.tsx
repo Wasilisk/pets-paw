9f5f99617600efbe020b7dacf1366e9cf4aeef3d
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-02 19:55:12 +0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import GlobalStyles from './global-styles';
+import {MainMenu} from "./components/MainMenu";
+import {BrowserRouter} from "react-router-dom";
 
-function App() {
+const App = () => {
     return (
         <React.StrictMode>
-            <></>
+            <BrowserRouter>
+                <GlobalStyles/>
+                <MainMenu/>
+            </BrowserRouter>
         </React.StrictMode>
     );
 }
