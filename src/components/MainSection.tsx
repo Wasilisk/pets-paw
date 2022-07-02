2aaa2d307a9b1fb8a7f5892b741d898c90ce626d
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-02 21:27:29 +0300

    feat: add main section
    This section will be displayed regardless of URL path.

---
+++ src/components/MainSection.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from "styled-components";
+import Logo from '../assets/logo/Logo.svg'
+import {MainMenu} from "./MainMenu";
+
+const MainContainer = styled.div`
+  width: 446px;
+  height: 544px;
+  top: 30px;
+  position: sticky;
+  margin-left: 117px;
+
+  .title {
+    font-weight: 500;
+    font-size: 44px;
+    line-height: 58px;
+    color: #1D1D1D;
+    margin-top: 80px;
+  }
+  
+  .description {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 29px;
+    color: #8C8C8C;
+    margin-top: 10px;
+    margin-bottom: 60px;
+  }
+  
+  .menu_title {
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 29px;
+    color: #1D1D1D;
+    margin-bottom: 20px;
+  }
+`
+
+const MainSection = () => {
+    return (
+        <MainContainer>
+            <img src={Logo} alt="PetsPaw logo"/>
+            <h4 className="title">Hi intern!</h4>
+            <p className="description">Welcome to MI 2022 Front-end test</p>
+            <p className="menu_title">Lets start using The Cat API</p>
+            <MainMenu/>
+        </MainContainer>
+    );
+};
+
+export default MainSection;
