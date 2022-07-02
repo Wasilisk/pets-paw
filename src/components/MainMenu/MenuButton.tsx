78ab8bf5a0b639c9c1b604b4e047f4648b758f1a
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-02 21:23:53 +0300

    feat: add menu button component

---
+++ src/components/MainMenu/MenuButton.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const MenuButton = styled.button`
+  width: 138px;
+  height: 36px;
+  margin-top: 10px;
+
+  background: #FFFFFF;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-family: 'Jost', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 2px;
+  color: #FF868E;
+  text-transform: uppercase;
+  transition: 0.2s ease;
+
+  &:hover {
+    background: #FBE0DC;
+  }
+
+  &:active {
+    background: #FF868E;
+    color: #FFFFFF;
+  }
+`;
