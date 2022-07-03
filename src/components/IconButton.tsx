d344b095ebb5e7b8a16142aae0492f991aa9f9dd
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-03 19:16:16 +0300

    feat: add IconButton component

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+type IconButtonElementProps = {
+    variant: "primary" | "secondary",
+}
+
+type IconButtonProps = IconButtonElementProps & {children?: React.ReactNode};
+
+const IconButtonElement = styled.button<IconButtonElementProps>`
+  width: 40px;
+  height: 40px;
+  border: none;
+  background: ${({variant}) => "primary" ? "#FBE0DC" : "#FFFFFF"};
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: 0.2s all;
+  
+  &:hover {
+    background: #FF868E;
+
+    path {
+      fill: #FFFFFF;
+    }
+  }
+`
+
+export const IconButton = ({variant, children}: IconButtonProps) => {
+    const navigate = useNavigate();
+    const historyBack = () => navigate(-1);
+
+    return (
+        <IconButtonElement variant={variant} onClick={historyBack}>
+            {children}
+        </IconButtonElement>
+    );
+};
