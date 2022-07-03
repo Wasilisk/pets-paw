8948c238575605d7c258bab9aedd8c579605b061
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-03 19:13:10 +0300

    feat: add IconLink component

---
+++ src/components/IconLink.tsx
@@ -0,0 +1,33 @@
+import {NavLink} from "react-router-dom";
+import styled from "styled-components";
+
+export const IconLink = styled(NavLink)`
+  width: 60px;
+  height: 60px;
+  background: #FFFFFF;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.2s all;
+  
+  &:hover {
+    background: #FBE0DC;
+  }
+  
+  &:active {
+    background: #FF868E;
+    
+    path {
+      fill: #FFFFFF;
+    }
+  }
+  
+  .active {
+    background: #FF868E;
+
+    path {
+      fill: #FFFFFF;
+    }
+  }
+`;
