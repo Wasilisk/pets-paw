6812d16b10649e5910c071c717684cfd32aa6938
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-03 17:10:32 +0300

    fix: change svg declaration
    Svg images are available as react components

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,8 @@ declare module 'module-name';
 declare module '*.jpg';
 declare module '*.png';
 declare module "*.svg" {
-    const content: any;
-    export default content;
+    import React = require('react');
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
 }
