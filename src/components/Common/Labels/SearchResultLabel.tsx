6f704d0e2df54735bb117baf5e0df753215aa24d
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-06 12:16:59 +0300

    feat: add label components for search result

---
+++ src/components/Common/Labels/SearchResultLabel.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components";
+
+const SearchResultLabelContainer = styled.p`
+  margin-top: 20px;
+  font-size: 20px;
+  line-height: 29px;
+  color: #8C8C8C;
+  
+  span {
+    color: #1D1D1D;
+    font-weight: 500;
+  }
+`
+
+type SearchResultLabelProps = {
+    breedName: string| null
+}
+
+export const SearchResultLabel = ({breedName}: SearchResultLabelProps) => {
+    return (
+        <SearchResultLabelContainer>
+            Search results for: <span>{breedName}</span>
+        </SearchResultLabelContainer>
+    );
+};
