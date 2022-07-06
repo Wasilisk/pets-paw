339bf48e81a91c50d591920a604ae9737a9b4ffe
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-06 11:53:13 +0300

    feat: add new routes for breed search ang breed info pages

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 import {Navigate, Route, Routes} from 'react-router-dom';
 
 /*components*/
-import HomeBanner from '../components/HomeBanner';
 import VotingPage from '../pages/Voting';
+import Breeds from "../pages/Breeds";
+import BreedInfo from '../pages/BreedInfo';
+import BreedSearch from "../pages/BreedSearch";
+import HomeBanner from "../components/HomeBanner";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route index element={<HomeBanner />} />
             <Route path="voting" element={<VotingPage/>}/>
-            <Route path="breeds" element={<VotingPage/>}/>
+            <Route path="breeds" element={<Breeds/>}/>
+            <Route path="breeds/:breedId" element={<BreedInfo/>}/>
+            <Route path="search" element={<BreedSearch/>}/>
             <Route path="gallery" element={<h4>Gallery</h4>}/>
             <Route path="likes" element={<h4>likes</h4>}/>
             <Route path="favourites" element={<h4>favourites</h4>}/>
