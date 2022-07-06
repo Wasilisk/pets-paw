bbd8457722c20fd0a71602060b4a1a4c7aaba791
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-06 12:14:10 +0300

    feat: add label components for empty result

---
+++ src/components/Common/Labels/EmptyLabel.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const EmptyLabel = styled.div`
+  width: 100%;
+  height: 60px;
+  background: #F8F8F7;
+  border-radius: 10px;
+  padding: 18px 20px;
+
+  font-size: 16px;
+  line-height: 24px;
+  color: #8C8C8C;
+`
