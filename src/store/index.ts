fca7365779b68543d53417a5b1ccca0871393061
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-06 11:47:55 +0300

    feat: add persisted store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,37 @@
 /*node-modules*/
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 /*store*/
 import votingReducer from './slices/voting-slice';
 import logsReducer from './slices/logs-slice';
+import breedsReducer from './slices/breeds-slice';
+
+const reducers = combineReducers({
+    voting: votingReducer,
+    action_logs: logsReducer,
+    breeds: breedsReducer
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['breeds', 'action_logs']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 
 const store = configureStore({
-    reducer: {
-        voting: votingReducer,
-        action_logs: logsReducer,
-    },
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST'],
+            },
+        }),
 })
 
 export type RootState = ReturnType<typeof store.getState>;
