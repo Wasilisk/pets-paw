99413e3c250da881d59f8724202efef824215b5a
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-09 14:30:31 +0300

    feat: add image grid item components

---
+++ src/components/Common/Grid/ImageGridItem.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ImageGridItem = styled.img`
+  border-radius: 20px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
--- src/components/Common/Grid/index.ts
+++ src/components/Common/Grid/index.ts
@@ -1,2 +1,5 @@
 export * from './ImageGrid';
 export * from './BreedGridItem';
+export * from './ImageGridItem';
+export * from './FavouriteImageGridItem';
+export * from './GalleryImageGridItem';
