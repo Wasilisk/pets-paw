98f24d56dee27556078ab654a7c5a9d96414ca3b
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-11 13:24:32 +0300

    fix: misspelled generic

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,7 +3,7 @@ declare module '*.jpg';
 declare module '*.png';
 declare module "*.svg" {
     import React = require('react');
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
     const src: string;
     export default src;
 }
